3f8f2e59b759a66b778e8e359778d7202caa96b2
Ian <ian@gmail.com>
2025-11-16 20:10:47 -0500

    Crear vista Login con navegación a Dashboard

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Página de Login</h1>
+      <Link to="/dashboard">Ir al Dashboard</Link>
+    </div>
+  );
+}
+
+
+
